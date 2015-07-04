b271ca0aa7aa85b44552d897e5108f0c32181c56
leetenki <toukyouniversityoftechnology@gmail.com>
2015-07-05 07:50:57 +0900

    add new feature modal window

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 window.onload = function() {
+
+	/******************* process email ******************/
+	$('#exampleModal').on('show.bs.modal', function (event) {
+	    var button = $(event.relatedTarget) // Button that triggered the modal
+	    var recipient = button.data('whatever') // Extract info from data-* attributes
+	    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+	    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+	    var modal = $(this)
+	    modal.find('.modal-title').text('New message to ' + recipient)
+	    modal.find('.modal-body input').val(recipient)
+	});
+
+	// event processor
+	$('button#submit').click(function(e){
+		console.log($("#recipient-name")[0].value);
+		console.log($("#message-text")[0].value);
+		$('#exampleModal').modal('hide');
+	});
+
+
+	/******************* process video ******************/
 	// turn of all videos
 	videos = document.getElementsByTagName("video");
 	for(var i = 1; i < videos.length; i++) {
 		videos[i].volume = 0;
 	}
-
 
 	// current and next item
 	var currentItem;
